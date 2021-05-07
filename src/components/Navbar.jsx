b0ead02fc4496894d206b4550ec386ef03297b7a
redoutable95 <broaccaunt@gmail.com>
2021-05-07 14:21:04 +0300

    git commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+const Nav = () => {
+  return (
+    <nav className="app-navigation">
+      <ul>
+        <a>
+          <li>Profile</li>
+        </a>
+        <a>
+          <li>Messages</li>
+        </a>
+        <a>
+          <li>News</li>
+        </a>
+        <a>
+          <li>Music</li>
+        </a>
+        <a>
+          <li>Settings</li>
+        </a>
+      </ul>
+    </nav>
+  );
+};
+export default Nav;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+const Profile = () => {
+  return (
+    <main className="app-maincontent">
+      <div className="cover">
+        <img
+          src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+          alt=""
+        />
+      </div>
+      <div className="avater">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwgkQqqZeEyj9J1dwi9aTwNaDTVaPWcBL28Q&usqp=CAU"></img>
+      </div>
+    </main>
+  );
+};
+export default Profile;
